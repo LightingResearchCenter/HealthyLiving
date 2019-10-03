457fb09246a5b42e77be701ecf910114dd014000
MichaelvMorrison <morrim5@rpi.edu>
2019-10-03 15:21:50 -0400

    accordion now only lets you open one thing from each tier at a time

--- js/accordion.js
+++ js/accordion.js
@@ -1,15 +1,49 @@
+function closeOthersLevel1(label){
+	var labels = $('.acnav__label');
+	for(var i = 0; i < labels.length; i++){
+		if ($(labels[i]) != label && !$(label).hasClass('acnav__label--level2')){
+			var parent = $(labels[i]).parent('.has-children');
+			var list = $(labels[i]).siblings('.acnav__list');
+			if (parent.hasClass('is-open')){
+				list.slideUp('fast');
+				parent.removeClass('is-open');
+			}
+		}
+	}
+}
+
+function closeOthersLevel2(label){
+	var labels = $('.acnav__label--level2');
+	for(var i = 0; i < labels.length; i++){
+		if ($(labels[i]) != label && $(label).hasClass('acnav__label--level2')){
+			var parent = $(labels[i]).parent('.has-children');
+			var list = $(labels[i]).siblings('.acnav__list');
+			if(parent.hasClass('is-open')){
+				list.slideUp('fast');
+				parent.removeClass('is-open');
+			}
+		}
+	}
+}
+
 // From https://codepen.io/byrnecode/pen/GxdQdQ
 $('.acnav__label').click(function () {
 	var label = $(this);
 	var parent = label.parent('.has-children');
 	var list = label.siblings('.acnav__list');
 
-	if ( parent.hasClass('is-open') ) {
+	if (parent.hasClass('is-open')) {
 		list.slideUp('fast');
 		parent.removeClass('is-open');
 	}
 	else {
+		if (label.hasClass('acnav__label--level2')){
+			closeOthersLevel2(label);
+		}else{
+			closeOthersLevel1(label);
+		}
 		list.slideDown('fast');
 		parent.addClass('is-open');
 	}
 });
+// From https://codepen.io/byrnecode/pen/GxdQdQ
