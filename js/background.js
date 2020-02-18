39c94bee669ad08040cfebfd69acf2374578b670
MichaelvMorrison <morrim5@rpi.edu>
2020-02-18 12:01:27 -0500

    Scroll back to top if nav bar is click? We talked about this before but maybe that's undesirable if it's accidentally clicked?

--- js/background.js
+++ js/background.js
@@ -161,4 +161,22 @@ $(document).ready(function(){
     }
   });
 
+
+  //https://codepen.io/matthewcain/pen/ZepbeR
+  var bTT = $('#backToTop');
+
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > 300) {
+      bTT.addClass('show');
+    } else {
+      bTT.removeClass('show');
+    }
+  });
+
+  bTT.on('click', function(e) {
+    e.preventDefault();
+    $('#content').scrollTop(0);
+    $('html, body').animate({scrollTop:0}, '300');
+  });
+
 });
